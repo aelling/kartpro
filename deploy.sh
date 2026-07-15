883cebc50e4a701fb0e8b6b44c778fb826555957
Austin Elling <aelling@gmail.com>
2026-07-14 21:38:10 -0500

    deploy: use full drush path in config import

--- deploy.sh
+++ deploy.sh
@@ -4,18 +4,16 @@ VPS_USER="dh_xsmc68"
 VPS_HOST="vps51963.dreamhostps.com"
 SITE_PATH="/home/dh_xsmc68/hivesticks.com"
 SSH_KEY="$HOME/.ssh/kartpro_deploy"
-DRUSH="php $SITE_PATH/vendor/drush/drush/drush.php"
 echo "Deploying to VPS..."
 ssh -i $SSH_KEY $VPS_USER@$VPS_HOST << ENDSSH
   set -e
   cd $SITE_PATH
-  # Preserve settings.php (prod Stripe keys + config_sync_directory)
   mv sites/default/settings.php /tmp/settings.php.bak
   git fetch origin
   git reset --hard origin/main
   cp /tmp/settings.php.bak sites/default/settings.php
-  \$DRUSH updatedb -y
-  \$DRUSH config:import -y
-  \$DRUSH cache:rebuild
+  php $SITE_PATH/vendor/drush/drush/drush.php updatedb -y
+  php $SITE_PATH/vendor/drush/drush/drush.php config:import -y
+  php $SITE_PATH/vendor/drush/drush/drush.php cache:rebuild
   echo "Deploy complete!"
 ENDSSH
