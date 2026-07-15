67c1ea612173e015896722719cbb83192a541713
Austin Elling <aelling@gmail.com>
2026-07-14 21:34:40 -0500

    deploy: import config + enable modules on deploy

--- deploy.sh
+++ deploy.sh
@@ -1,24 +1,21 @@
 #!/bin/bash
 set -e
-
 VPS_USER="dh_xsmc68"
 VPS_HOST="vps51963.dreamhostps.com"
 SITE_PATH="/home/dh_xsmc68/hivesticks.com"
 SSH_KEY="$HOME/.ssh/kartpro_deploy"
-
+DRUSH="php $SITE_PATH/vendor/drush/drush/drush.php"
 echo "Deploying to VPS..."
-
 ssh -i $SSH_KEY $VPS_USER@$VPS_HOST << ENDSSH
+  set -e
   cd $SITE_PATH
-  # Preserve settings.php
+  # Preserve settings.php (prod Stripe keys + config_sync_directory)
   mv sites/default/settings.php /tmp/settings.php.bak
-  # Sync with GitHub
   git fetch origin
   git reset --hard origin/main
-  # Restore settings.php
   cp /tmp/settings.php.bak sites/default/settings.php
-  # Run Drupal updates
-  php $SITE_PATH/vendor/drush/drush/drush.php cache:rebuild
-  php $SITE_PATH/vendor/drush/drush/drush.php updatedb -y
+  \$DRUSH updatedb -y
+  \$DRUSH config:import -y
+  \$DRUSH cache:rebuild
   echo "Deploy complete!"
 ENDSSH
